cbf1151d7c3ceb5eaea9aa25704186243d1e28f6
resilienciaeth <106503220+resilienciaeth@users.noreply.github.com>
2022-12-23 20:47:31 -0500

    server-changes

--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(500, () => console.log('AI server started on http://localhost:500'))
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
